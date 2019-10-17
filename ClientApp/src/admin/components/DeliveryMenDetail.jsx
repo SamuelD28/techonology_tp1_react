66bf806a8985d82f624441ecb413d2c96190adb7
1489005 <1489005@cstjean.qc.ca>
2019-10-17 15:08:28 -0400

    Wip : Confirm modal

--- ClientApp/src/admin/components/DeliveryMenDetail.jsx
+++ ClientApp/src/admin/components/DeliveryMenDetail.jsx
@@ -1,6 +1,7 @@
 ﻿import React from 'react';
 import Ajax from '../../shared/ajax';
 import { Form, Row, Col, FormGroup, Input, Label, Button } from 'reactstrap';
+import Confirm from '../../shared/components/Confirm';
 
 class DeliveryMenDetail extends React.Component {
 
@@ -57,69 +58,87 @@ class DeliveryMenDetail extends React.Component {
 
     render() {
         return (
-            <Form>
-                <h1 className="text-primary">{this.props.deliveryMan && this.props.deliveryMan.name}</h1>
-                <FormGroup>
-                    <Label htmlFor="exampleEmail">Nom</Label>
-                    <Input
-                        type="text"
-                        id="inputName"
-                        placeholder="Nom..."
-                        value={this.state.name}
-                        onChange={(e) => this.HandleChange("name", e.target.value)}
-                    />
-                </FormGroup>
-                <FormGroup>
-                    <Label htmlFor="exampleAddress">Téléphone</Label>
-                    <Input
-                        type="phone"
-                        id="inputPhone"
-                        placeholder="Téléphone..."
-                        value={this.state.phone}
-                        onChange={(e) => this.HandleChange("phone", e.target.value)}
-                    />
-                </FormGroup>
-                <FormGroup>
-                    <Input
-                        className="inp-cbx"
-                        id="isEmployed"
-                        style={{ display: "none" }}
-                        type="checkbox"
-                        checked={this.state.isEmployed}
-                        onChange={(e) => this.HandleChange("isEmployed", !this.state.isEmployed)}
-                    />
-                    <label className="cbx" htmlFor="isEmployed">
-                        <span>
-                            <svg width="12px" height="10px" viewBox="0 0 12 10">
-                                <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
-                            </svg>
-                        </span>
-                        <span>Employé</span>
-                    </label>
-                </FormGroup>
-                <FormGroup>
-                    <Input
-                        className="inp-cbx"
-                        id="isDeactivated"
-                        style={{ display: "none" }}
-                        type="checkbox"
-                        checked={this.state.isDeactivated}
-                        onChange={(e) => this.HandleChange("isDeactivated", !this.state.isDeactivated)}
-                    />
-                    <label className="cbx" htmlFor="isDeactivated">
-                        <span>
-                            <svg width="12px" height="10px" viewBox="0 0 12 10">
-                                <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
-                            </svg>
-                        </span>
-                        <span>Désactivé</span>
-                    </label>
-                </FormGroup>
+            <div>
+                <Form>
+                    <h1 className="text-primary">{this.props.deliveryMan && this.props.deliveryMan.name}</h1>
+                    <FormGroup>
+                        <Label htmlFor="exampleEmail">Nom</Label>
+                        <Input
+                            type="text"
+                            id="inputName"
+                            placeholder="Nom..."
+                            value={this.state.name}
+                            onChange={(e) => this.HandleChange("name", e.target.value)}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label htmlFor="exampleAddress">Téléphone</Label>
+                        <Input
+                            type="phone"
+                            id="inputPhone"
+                            placeholder="Téléphone..."
+                            value={this.state.phone}
+                            onChange={(e) => this.HandleChange("phone", e.target.value)}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Input
+                            className="inp-cbx"
+                            id="isEmployed"
+                            style={{ display: "none" }}
+                            type="checkbox"
+                            checked={this.state.isEmployed}
+                            onChange={(e) => this.HandleChange("isEmployed", !this.state.isEmployed)}
+                        />
+                        <label className="cbx" htmlFor="isEmployed">
+                            <span>
+                                <svg width="12px" height="10px" viewBox="0 0 12 10">
+                                    <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
+                                </svg>
+                            </span>
+                            <span>Employé</span>
+                        </label>
+                    </FormGroup>
+                    <FormGroup>
+                        <Input
+                            className="inp-cbx"
+                            id="isDeactivated"
+                            style={{ display: "none" }}
+                            type="checkbox"
+                            checked={this.state.isDeactivated}
+                            onChange={(e) => this.HandleChange("isDeactivated", !this.state.isDeactivated)}
+                        />
+                        <label className="cbx" htmlFor="isDeactivated">
+                            <span>
+                                <svg width="12px" height="10px" viewBox="0 0 12 10">
+                                    <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
+                                </svg>
+                            </span>
+                            <span>Désactivé</span>
+                        </label>
+                    </FormGroup>
+                </Form>
                 <div className="d-flex justify-content-between mt-4">
-                    <Button onClick={this.HandleDelete} color="danger">Supprimer</Button>
-                    <Button onClick={this.HandleUpdate} color="success">Sauvgarder</Button>
+                    <Confirm
+                        trigger={
+                            <Button
+                                color="danger">
+                                Supprimer
+                            </Button>
+                        }
+                        successAction={this.HandleDelete}
+                    />
+                    <Confirm
+                        trigger={
+                            <Button
+                                color="success">
+                                Sauvgarder
+                            </Button>
+                        }
+                        successAction={this.HandleUpdate}
+                    />
                 </div>
-            </Form>
+            </div>
         );
     }
 }
---
+++ ClientApp/src/shared/components/Confirm.jsx
@@ -0,0 +1,53 @@
+﻿import React, { Component } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
+class Confirm extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { modal: false };
+    }
+
+    Toggle = () => this.setState({ modal: !this.state.modal });
+
+    ExecuteConfirmedAction = (confirmed) => {
+        if (confirmed) {
+            this.props.successAction();
+            this.setState({ modal: false });
+        } else {
+            this.setState({ modal: false });
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <span onClick={this.Toggle}>
+                    {this.props.trigger}
+                </span>
+                <Modal
+                    isOpen={this.state.modal}
+                    toggle={this.Toggle}>
+                    <ModalBody className="bg-dark">
+                        <h3 className="text-primary">Confirmer</h3>
+                        <p>Êtes-vous sûr de vouloir continuer?</p>
+                        <div>
+                            <Button
+                                color="success"
+                                onClick={() => this.ExecuteConfirmedAction(true)}>
+                                Oui
+                            </Button>
+                            <Button
+                                color="danger"
+                                onClick={() => this.ExecuteConfirmedAction(false)}>
+                                Non
+                            </Button>
+                        </div>
+                        </ModalBody>
+                </Modal>
+            </div>
+        )
+    }
+};
+
+export default Confirm;
