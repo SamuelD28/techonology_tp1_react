7e00c1f5f3ccc093e4ae6dc9a4342291ed8104fb
Maxime Boisvert <Maxiimeeb@hotmail.com>
2019-12-04 18:35:48 -0500

    Stripe

--- Program.cs
+++ Program.cs
@@ -20,5 +20,6 @@ namespace Technology_Tp1_React
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+
     }
 }
